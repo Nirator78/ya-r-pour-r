2d268d43d87e17007a7815794cd3d39d7f6e63f5
nolan78 <superpika.nolan@gmail.com>
2022-12-20 15:05:00 +0100

    Amelioration du csv

--- cleaner.r
+++ cleaner.r
@@ -6,5 +6,7 @@ df <- na.omit(df)
 # Retirer les lignes avec toutes les valeurs a 0 (sauf les 3 premières)
 df <- df[!apply(df[-1:-3], 1, function(x) all(x == 0)), ]
 
+# Order by date
+df <- df[order(df$jour), ]
 # Creation d'un nouveau csv
-write.csv(df, "data_clean.csv", row.names = FALSE)
+write.csv2(df, "data_clean.csv", row.names = FALSE, quote = FALSE)
