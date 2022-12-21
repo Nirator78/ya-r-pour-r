76827a8e33d8652c14c41a8fd3944ea371fd0c95
Nirator78 <51956876+Nirator78@users.noreply.github.com>
2022-12-21 16:15:40 +0100

    reticence par région Close #4

---
+++ reticence-region.r
@@ -0,0 +1,44 @@
+library(ggplot2)
+library(dplyr)
+library(scales)
+
+# Import csv
+df <- read.csv("data_clean.csv", header = TRUE, sep = ";")
+
+# Transform date to int (number of days since 1970-01-01)
+df$jour <- as.Date(df$jour, format = "%Y-%m-%d")
+df$date <- as.integer(df$jour)
+
+# Delete row with reg = 0
+df <- subset(df, reg != 0)
+
+df$n_cum_dose1 <- df$n_cum_dose1_h + df$n_cum_dose1_f
+
+# Group by date and reg
+df <- df %>%
+  group_by(date, jour, reg) %>%
+  summarise(n_cum_dose1 = sum(n_cum_dose1))
+
+# Transform n_cum_dose1 to percentage by age
+df2 <- df %>% group_by(reg) %>% summarise(max = (max(n_cum_dose1)))
+
+df <- merge(x = df, y = df2, by = "reg", all.x = TRUE)
+
+df$n_cum_dose1 <- ((as.double(100)*df$n_cum_dose1) / df$max)
+
+ggplot(data = df, aes(jour, n_cum_dose1, group = reg)) +
+  geom_line(aes(color = as.factor(reg)), linewidth = 1.2) +
+  geom_label(aes(label = reg),
+              data = df %>% filter(date == median(df$date)),
+              nudge_x = 0.35,
+              size = 2) +
+  scale_x_date(
+    labels = date_format(format = "%m-%Y"),
+    breaks = date_breaks("2 month")
+  ) +
+  ggtitle("% de vaccination par region") +
+  ylab("Pourcentage de la population vaccinée") +
+  xlab("Date") +
+  theme(axis.text.x = element_text(angle = 60, hjust = 1)) +
+  theme(legend.position = "bottom")
+  ggsave("reticence-region.png", width = 100, height = 60, units = "cm", dpi = 300)
