2461e341b9a47507d8feca5542299f949d82ff5f
nolan78 <superpika.nolan@gmail.com>
2022-12-22 15:27:09 +0100

    Femme /Homme

--- reticance_par_sexe.r
+++ reticance_par_sexe.r
@@ -73,36 +73,54 @@ ggsave("AnneeMoisRegression.png", bg = "white")
 # Si augmentation des doses femmes, il y a augmentation des doses hommes
 # Si augmentation des doses hommes, il y a augmentation des doses femmes
 
-# Un modele de regression lineaire avec lm
-modele <- lm(dfAnneeMoisFemme$cum_dose1_femme_annee_mois ~ dfAnneeMoisHomme$cum_dose1_homme_annee_mois, data = dfAnneeMoisHomme)
-# prediction de la valeur de la dose 1 pour les hommes
-predict(modele, data.frame(dfAnneeMoisHomme$cum_dose1_homme_annee_mois == 1000000))
-# prediction de la valeur de la dose 1 pour les femmes
-predict(modele, data.frame(dfAnneeMoisFemme$cum_dose1_femme_annee_mois == 1000000))
-
-
-View(dfAnneeMoisHomme)
 # Crée une colone regroupent le mois et l'année de AnneeMoisHomme
-
 dfAnneeMoisHomme$mois_annee <- paste(dfAnneeMoisHomme$annee, dfAnneeMoisHomme$mois, 1, sep = "-")
 dfAnneeMoisHomme$mois_annee <- as.Date(dfAnneeMoisHomme$mois_annee, format = "%Y-%m-%d")
 View(dfAnneeMoisHomme)
+
+dfAnneeMoisFemme$mois_annee <- paste(dfAnneeMoisFemme$annee, dfAnneeMoisFemme$mois, 1, sep = "-")
+dfAnneeMoisFemme$mois_annee <- as.Date(dfAnneeMoisFemme$mois_annee, format = "%Y-%m-%d")
+View(dfAnneeMoisFemme)
 # Un modele pour prevoir le nombre de doses 1 pour les hommes en 2023
-modele2 <- lm(cum_dose1_homme_annee_mois ~ poly(mois_annee,1), data = dfAnneeMoisHomme)
+modeleHomme <- lm(cum_dose1_homme_annee_mois ~ poly(mois_annee,1), data = dfAnneeMoisHomme)
 # prediction du nombre de doses 1 pour les hommes en 2023
-new <- data.frame(mois_annee = as.Date(c("2023-01-01","2023-02-01","2023-03-01","2023-04-01","2023-05-01","2023-06-01","2023-07-01","2023-08-01","2023-09-01","2023-10-01","2023-11-01","2023-12-01")))
-pred <- predict(modele2, new, interval = "prediction")
-new <- data.frame(new)
-pred <- data.frame(pred)
+newHomme <- data.frame(mois_annee = as.Date(c("2023-01-01","2023-02-01","2023-03-01","2023-04-01","2023-05-01","2023-06-01","2023-07-01","2023-08-01","2023-09-01","2023-10-01","2023-11-01","2023-12-01")))
+predHomme <- predict(modeleHomme, newHomme, interval = "prediction")
+predHomme <- data.frame(predHomme)
+newHomme <- data.frame(newHomme)
 
 # Prendre le range entre fit et upr
-pred$range <- (pred$upr + pred$fit) /2
-View(pred)
+predHomme$range <- (predHomme$upr + predHomme$fit) /2
+View(predHomme)
 
+# Generation du graphique de prediction homme
 png(file="prediction_2023_homme.png")
 plot(dfAnneeMoisHomme$mois_annee, dfAnneeMoisHomme$cum_dose1_homme_annee_mois, col = "blue", pch = 20, cex = 1.5, xlab = "Mois", ylab = "Nombre de doses", main = "Evolution des doses par mois pour les hommes", xlim= c(as.Date('2020-12-01'), as.Date('2023-12-01')))
 lines(dfAnneeMoisHomme$mois_annee, dfAnneeMoisHomme$cum_dose1_homme_annee_mois, col = "blue", pch = 20, cex = 1.5, xlab = "Mois", ylab = "Nombre de doses", main = "Evolution des doses par mois pour les hommes", xlim= c(as.Date('2020-12-01'), as.Date('2023-12-01')))
-points(new$mois_annee, pred$fit, col = "green")
-points(new$mois_annee, pred$upr, col = "orange")
-points(new$mois_annee, pred$range, col = "red")
+points(newHomme$mois_annee, predHomme$fit, col = "green")
+points(newHomme$mois_annee, predHomme$upr, col = "orange")
+points(newHomme$mois_annee, predHomme$range, col = "red")
 dev.off()
+
+# Le meme modele pour prevoir le nombre de doses 1 pour les femmes en 2023
+modelFemme <- lm(cum_dose1_femme_annee_mois ~ poly(mois_annee,1), data = dfAnneeMoisFemme)
+# prediction du nombre de doses 1 pour les femmes en 2023
+newFemme <- data.frame(mois_annee = as.Date(c("2023-01-01","2023-02-01","2023-03-01","2023-04-01","2023-05-01","2023-06-01","2023-07-01","2023-08-01","2023-09-01","2023-10-01","2023-11-01","2023-12-01")))
+predFemme <- predict(modelFemme, newFemme, interval = "prediction")
+newFemme <- data.frame(newFemme)
+predFemme <- data.frame(predFemme)
+
+# Prendre le range entre fit et upr
+predFemme$range <- (predFemme$upr + predFemme$fit) /2
+View(predFemme)
+
+# Generation du graphique de prediction femme
+png(file="prediction_2023_femme.png")
+plot(dfAnneeMoisFemme$mois_annee, dfAnneeMoisFemme$cum_dose1_femme_annee_mois, col = "blue", pch = 20, cex = 1.5, xlab = "Mois", ylab = "Nombre de doses", main = "Evolution des doses par mois pour les femmes", xlim= c(as.Date('2020-12-01'), as.Date('2023-12-01')))
+lines(dfAnneeMoisFemme$mois_annee, dfAnneeMoisFemme$cum_dose1_femme_annee_mois, col = "blue", pch = 20, cex = 1.5, xlab = "Mois", ylab = "Nombre de doses", main = "Evolution des doses par mois pour les femmes", xlim= c(as.Date('2020-12-01'), as.Date('2023-12-01')))
+points(newFemme$mois_annee, predFemme$fit, col = "green")
+points(newFemme$mois_annee, predFemme$upr, col = "orange")
+points(newFemme$mois_annee, predFemme$range, col = "red")
+dev.off()
+
+
