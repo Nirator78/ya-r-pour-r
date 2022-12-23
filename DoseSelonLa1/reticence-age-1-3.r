486f76e718e61a3f05c66e1b447f11558204f368
Nirator78 <51956876+Nirator78@users.noreply.github.com>
2022-12-23 14:30:16 +0100

    comparaison entre les doses pas fini mais la

---
+++ DoseSelonLa1/reticence-age-1-3.r
@@ -0,0 +1,45 @@
+library(ggplot2)
+library(dplyr)
+library(scales)
+
+# Import csv
+df <- read.csv("data_clean.csv", header = TRUE, sep = ";")
+
+# Transform date to int (number of days since 1970-01-01)
+df$jour <- as.Date(df$jour, format = "%Y-%m-%d")
+df$date <- as.integer(df$jour)
+
+# Delete row with clage_vacsi = 0
+df <- subset(df, clage_vacsi != 0)
+
+df$n_cum_dose1 <- df$n_cum_dose1_h + df$n_cum_dose1_f
+df$n_cum_rappel <- df$n_3_rappel_h + df$n_3_rappel_h
+
+# Group by date and clage_vacsi
+df <- df %>%
+  group_by(date, jour, clage_vacsi) %>%
+  summarise(n_cum_rappel = sum(n_cum_rappel), n_cum_dose1 = sum(n_cum_dose1))
+
+# Transform n_cum_rappel to percentage by age
+df2 <- df %>% group_by(clage_vacsi) %>% summarise(max = (max(n_cum_dose1)))
+
+df <- merge(x = df, y = df2, by = "clage_vacsi", all.x = TRUE)
+
+df$n_cum_rappel <- ((as.double(100)*df$n_cum_rappel) / df$max)
+
+ggplot(data = df, aes(jour, n_cum_rappel, group = clage_vacsi)) +
+  geom_line(aes(color = as.factor(clage_vacsi)), linewidth = 1.2) +
+  geom_label(aes(label = clage_vacsi),
+              data = df %>% filter(date == median(df$date)),
+              nudge_x = 0.35,
+              size = 2) +
+  scale_x_date(
+    labels = date_format(format = "%m-%Y"),
+    breaks = date_breaks("2 month")
+  ) +
+  ggtitle("% de vaccination par tranche d'âge") +
+  ylab("Pourcentage de la population vaccinée") +
+  xlab("Date") +
+  theme(axis.text.x = element_text(angle = 60, hjust = 1)) +
+  theme(legend.position = "bottom")
+  ggsave("reticence-age.png", width = 100, height = 60, units = "cm", dpi = 300)
